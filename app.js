dcdc81bc7cd74d2907c691e05e9d262cf01aed37
Edgard Kozlowski <edgardfernandes11@gmail.com>
2017-11-09 14:34:31 -0200

    📦 update package.json and create bin

--- app.js
+++ app.js
@@ -11,48 +11,53 @@ const parseBadge = require('./src/parseBadge.js')
 
 // Start the code here
 
-setTitle('📰 dncli - CLI to browse designernews.co')
+const dncli = () => {
 
-scrapeIt(
-	process.argv[2] ? `https://www.designernews.co/badges/${process.argv[2]}` : 'https://www.designernews.co'
-	, {
-	stories: {
-		listItem: '.montana-list-item',
-		data: {
-			id: {
-				attr: 'data-story-id'
-			},
-			upvotes: '.upvoted-number',
-			title: {
-				selector: '.montana-item-title',
-				attr: 'alt'
-			},
-			url: {
-				selector: '.montana-item-title',
-				attr: 'href'
-			},
-			timeAgo: '.list-story-time-ago',
-			from: '.montana-item-meta span a'
+	setTitle('📰 dncli - CLI to browse designernews.co')
+
+	scrapeIt(
+		process.argv[2] ? `https://www.designernews.co/badges/${process.argv[2]}` : 'https://www.designernews.co'
+		, {
+		stories: {
+			listItem: '.montana-list-item',
+			data: {
+				id: {
+					attr: 'data-story-id'
+				},
+				upvotes: '.upvoted-number',
+				title: {
+					selector: '.montana-item-title',
+					attr: 'alt'
+				},
+				url: {
+					selector: '.montana-item-title',
+					attr: 'href'
+				},
+				timeAgo: '.list-story-time-ago',
+				from: '.montana-item-meta span a'
+			}
 		}
-	}
-	}).then(data => {
-		const storiesQuestions = {
-			type: 'list',
-			name: 'selectedStorie',
-			message: 'Select a story to read',
-			pageSize: 30,
-			choices: data.stories.map(item => {
-				return {
-					name: `${chalk.green('▴')} ${chalk.gray(`${ ('0' + Number(item.upvotes)).slice(-2)} |`)} ${item.title} ${chalk.gray(`- by ${item.from} (${item.timeAgo})`)}`,
-					value: item.id
+		}).then(data => {
+			const storiesQuestions = {
+				type: 'list',
+				name: 'selectedStorie',
+				message: 'Select a story to read',
+				pageSize: 30,
+				choices: data.stories.map(item => {
+					return {
+						name: `${chalk.green('▴')} ${chalk.gray(`${ ('0' + Number(item.upvotes)).slice(-2)} |`)} ${item.title} ${chalk.gray(`- by ${item.from} (${item.timeAgo})`)}`,
+						value: item.id
+					}
+				})
+			}
+			inquirer.prompt([storiesQuestions]).then(function (answer) {
+				for (let i = 0; i < data.stories.length; i++) {
+					if (data.stories[i].id === answer.selectedStorie) {
+						openurl.open(data.stories[i].url.includes('http') ? data.stories[i].url : 'https://www.designernews.co' + data.stories[i].url)
+					}
 				}
 			})
-		}
-		inquirer.prompt([storiesQuestions]).then(function (answer) {
-			for (let i = 0; i < data.stories.length; i++) {
-				if (data.stories[i].id === answer.selectedStorie) {
-					openurl.open(data.stories[i].url.includes('http') ? data.stories[i].url : 'https://www.designernews.co' + data.stories[i].url)
-				}
-			}
 		})
-	})
+}
+
+exports.dncli = dncli
