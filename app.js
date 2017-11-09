f381511b2f616c88b06c638a25bc9df7556c18c9
Edgard Kozlowski <edgardfernandes11@gmail.com>
2017-11-09 13:43:23 -0200

    📰 Top stories working

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Dependencies
-const scraperjs = require('scraperjs')
 const setTitle = require('node-bash-title')
+const scrapeIt = require("scrape-it")
+const inquirer = require("inquirer")
+const openurl = require('openurl')
+const chalk = require('chalk')
 
 // src
 const parseName = require('./src/parseName.js')
@@ -8,18 +11,48 @@ const parseBadge = require('./src/parseBadge.js')
 
 // Start the code here
 
-setTitle('designernews-cli')
+setTitle('📰 dncli - CLI to browse designernews.co')
 
-scraperjs.StaticScraper.create('https://www.designernews.co/')
-	.scrape(function($) {
-		return $('.montana-list-item').map(function() {
-			return {
-				id: $(this).attr('data-story-id'),
-				upvotes: $(this.childNodes[1].childNodes[0].childNodes[4]).text(),
-				badge: parseBadge($(this.childNodes[0].childNodes[0]).attr('href'))
-      }
-		}).get()
-	})
-	.then(function(news) {
-		console.log(news)
+scrapeIt(
+	'https://www.designernews.co'
+	, {
+	stories: {
+		listItem: '.montana-list-item',
+		data: {
+			id: {
+				attr: 'data-story-id'
+			},
+			upvotes: '.upvoted-number',
+			title: {
+				selector: '.montana-item-title',
+				attr: 'alt'
+			},
+			url: {
+				selector: '.montana-item-title',
+				attr: 'href'
+			},
+			timeAgo: '.list-story-time-ago',
+			from: '.montana-item-meta span a'
+		}
+	}
+	}).then(data => {
+		const storiesQuestions = {
+			type: 'list',
+			name: 'selectedStorie',
+			message: 'Select a story to read',
+			pageSize: 30,
+			choices: data.stories.map(item => {
+				return {
+					name: `${chalk.green('▴')} ${chalk.gray(`${ ('0' + Number(item.upvotes)).slice(-2)} |`)} ${item.title} ${chalk.gray(`- by ${item.from} (${item.timeAgo})`)}`,
+					value: item.id
+				}
+			})
+		}
+		inquirer.prompt([storiesQuestions]).then(function (answer) {
+			for (let i = 0; i < data.stories.length; i++) {
+				if (data.stories[i].id === answer.selectedStorie) {
+					openurl.open(data.stories[i].url)
+				}
+			}
+		})
 	})
